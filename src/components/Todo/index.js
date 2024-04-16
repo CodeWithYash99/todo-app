ba76a297f84f5b647e51a10a3d36f5dca4124131
CodeWithYash99 <yashwanthrangu99@gmail.com>
2024-04-16 20:56:04 +0530

    Update Todo Items

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,34 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 import { TodoItem } from "../TodoItem";
 
 import "./index.css";
 
+const initialTodoItems = () => {
+  let getLocalItems = localStorage.getItem("todosList");
+
+  if (getLocalItems) {
+    return JSON.parse(localStorage.getItem(getLocalItems));
+  } else {
+    return [];
+  }
+};
+
 export const Todo = () => {
   const [todo, setTodo] = useState("");
   const [todoItems, setTodoItems] = useState([]);
+  const [updateTodo, setUpdateTodo] = useState(initialTodoItems());
+
+  useEffect(() => {
+    localStorage.setItem("todosList", JSON.stringify(todoItems));
+  }, [todoItems]);
 
   function addTodoHandler(event) {
     event.preventDefault();
 
-    if (todo !== "") {
+    if (todo) {
       const newTodo = {
         id: uuidv4(),
         title: todo,
+        status: false,
       };
       setTodoItems((prev) => [...prev, newTodo]);
       setTodo("");
     }
   }
 
-  function onChangeTodoInput(event) {
-    setTodo(event.target.value);
-  }
-
-  function todoEdit(id) {
-    const edit = todoItems.filter((each) => each.id === id)
-    setTodo(edit[0].title)
+  function todoComplete(id) {
+    let newTodo = todoItems.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, status: !todo.status };
+      }
+      return todo;
+    });
+    setTodoItems(newTodo);
   }
 
   function deleteTodoItem(id) {
@@ -36,33 +53,76 @@ export const Todo = () => {
     setTodoItems(updateTodo);
   }
 
+  function onChangeUpdateTodo(e) {
+    let newEntry = {
+      id: updateTodo.id,
+      title: e.target.value,
+      status: updateTodo.status ? true : false,
+    };
+
+    setUpdateTodo(newEntry);
+  }
+
+  function onClickUpdateTodo() {
+    let filterData = [...todoItems].filter((todo) => todo.id !== updateTodo.id);
+    let filterRecords = [...filterData, updateTodo];
+    setTodoItems(filterRecords);
+    setUpdateTodo("");
+  }
+
+  function onClickCancelTodo(e) {
+    setUpdateTodo("");
+  }
+
   return (
     <div className="todo-container flex flex-col items-center">
       <h1 className="todo-title">Todo Application</h1>
 
-      <form
-        className="todo-form-container flex flex-row justify-evenly"
-        onSubmit={addTodoHandler}
-      >
-        <input
-          className="todo-input"
-          type="text"
-          placeholder="Enter Todo here..."
-          name="Todo"
-          value={todo}
-          onChange={onChangeTodoInput}
-        />
-        <input className="add-todo-btn" type="submit" value="Add Todo" />
-      </form>
+      {updateTodo ? (
+        <form
+          className="todo-form-container flex flex-row justify-evenly"
+          onSubmit={addTodoHandler}
+        >
+          <input
+            className="todo-update-input"
+            type="text"
+            name="Todo"
+            value={updateTodo && updateTodo.title}
+            onChange={onChangeUpdateTodo}
+          />
+          <button className="update-todo-btn" onClick={onClickUpdateTodo}>
+            Update
+          </button>
+          <button className="cancel-todo-btn" onClick={onClickCancelTodo}>
+            Cancel
+          </button>
+        </form>
+      ) : (
+        <form
+          className="todo-form-container flex flex-row justify-evenly"
+          onSubmit={addTodoHandler}
+        >
+          <input
+            className="todo-input"
+            type="text"
+            placeholder="Enter Todo here..."
+            name="Todo"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <input className="add-todo-btn" type="submit" value="Add Todo" />
+        </form>
+      )}
 
       {todoItems.length !== 0 ? (
         <TodoItem
           todoItems={todoItems}
-          todoEdit={todoEdit}
+          setUpdateTodo={setUpdateTodo}
+          todoComplete={todoComplete}
           deleteTodoItem={deleteTodoItem}
         />
       ) : (
-        ""
+        <p className="empty-todos">No Todos here...</p>
       )}
     </div>
   );
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 
 import { BiSolidEdit } from "react-icons/bi";
-import { IoTrashSharp } from "react-icons/io5";
+import { IoTrashSharp, IoCheckmarkDoneCircle } from "react-icons/io5";
+// import { GrUpdate } from "react-icons/gr";
 
 import "./index.css";
 
-export const TodoItem = (props) => {
-  const { todoItems, todoEdit, deleteTodoItem } = props;
-
+export const TodoItem = ({ todoItems, deleteTodoItem, todoComplete, setUpdateTodo }) => {
   return (
     <div className="todo-item-container flex flex-col justify-center items-center">
       <h3 className="todo-items-heading">Todo Items</h3>
@@ -18,12 +17,20 @@ export const TodoItem = (props) => {
             key={todo.id}
             className="todo-list-item flex flex-row justify-between items-center"
           >
-            <p>{todo.title}</p>
+            <p className={todo.status ? "done" : ""}>{todo.title}</p>
+
             <div className="icons-container flex flex-row justify-between">
-              <BiSolidEdit
-                className="todo-edit-icon"
-                onClick={() => todoEdit(todo.id)}
+              <IoCheckmarkDoneCircle
+                className="todo-complete-icon"
+                onClick={() => todoComplete(todo.id)}
               />
+
+              {todo.status ? null : <BiSolidEdit className="todo-edit-icon" onClick={() => setUpdateTodo({
+                id: todo.id,
+                title: todo.title,
+                status: todo.status ? true : false
+              })} />}
+
               <IoTrashSharp
                 className="todo-delete-icon"
                 onClick={() => deleteTodoItem(todo.id)}
